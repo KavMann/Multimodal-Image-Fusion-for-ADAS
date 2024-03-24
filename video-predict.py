d836ddfbd9306f9594f43ec7a284e4b4767d9e55
dhruv <dhruvpant116@gmail.com>
2024-03-25 02:32:15 +0530

    rgb video

--- video-predict.py
+++ video-predict.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Command to execute
-command = 'yolo task=detect mode=predict model="C:\\Users\\dhruv\\Desktop\\Capstone Project Multimodal Image Fusion\\Multimodal-Image-Fusion\\runs\\train-ir\\weights\\best.pt" source="C:\\Users\\dhruv\\Desktop\\Capstone Project Multimodal Image Fusion\\Multimodal-Image-Fusion\\videos\\IR\\set-9.mp4" conf=0.5 save=True'
+command = 'yolo task=detect mode=predict model="C:\\Users\\dhruv\\Desktop\\Capstone Project Multimodal Image Fusion\\Multimodal-Image-Fusion\\runs\\train-ir\\weights\\best.pt" source="C:\\Users\\dhruv\\Desktop\\Capstone Project Multimodal Image Fusion\\Multimodal-Image-Fusion\\videos\\IR\\set-9.mp4" conf=0.3 save=True'
 
 # Execute the command
 subprocess.run(command, shell=True)
